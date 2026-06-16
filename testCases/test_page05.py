a38bba12e1ed8da916777e87f9d1d9b0c21d431f
credrepo <credencefor@gmail.com>
2026-06-16 10:06:47 +0530

    Update test_page05.py

--- testCases/test_page05.py
+++ testCases/test_page05.py
@@ -1,10 +1,17 @@
-def test_verity_all_test(self, setup):
-    p5 = Page05(setup)
+import pytest
+from Pages.Page05 import Page05
 
-    p5.click_widget_btn()
-    p5.click_date_picker()
-    p5.select_picker()
-    p5.click_select_month()
-    p5.click_select_year()
-    p5.click_select_time()    # ✅ PEHLE
-    p5.click_select_date()    # ✅ BAAD MEIN
+
+@pytest.mark.usefixtures("setup")
+class Test_Pagefive:
+
+    def test_verity_all_test(self, setup):
+        p5 = Page05(setup)
+
+        p5.click_widget_btn()
+        p5.click_date_picker()
+        p5.select_picker()
+        p5.click_select_month()
+        p5.click_select_year()
+        p5.click_select_time()
+        p5.click_select_date()
